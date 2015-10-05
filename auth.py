9f66260376324f8dca451b039495c89607938509
Paul <prsephton@gmail.com>
2015-10-05 07:27:28 +0200

    white space

--- auth.py
+++ auth.py
@@ -178,7 +178,8 @@ class AuthenticatorPlugin(PrincipalFolder, grok.LocalUtility):
         '''
         super(AuthenticatorPlugin, self).__init__(*args, **kwargs)
 
-        su = InternalPrincipal(login='admin', password='Admin', title=u'Administrator', description=u'The SuperUser')
+        su = InternalPrincipal(login='admin', password='Admin', title=u'Administrator', 
+                               description=u'The SuperUser')
         self['admin'] = su
 
         roleMgr = IPrincipalRoleManager(grok.getSite())
